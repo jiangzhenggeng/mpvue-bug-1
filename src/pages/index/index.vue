6c514804f57738138493f08c54b40881b759f6ac
蒋正庚 <jiangzg@glodon.com>
2019-06-22 10:30:25 +0800

    -

---
+++ src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div @click="clickHandle">点击增加 currentIndex </div>
+    <block v-for="(item,index) in list" :key="index">
+      <test
+        v-if="index==currentIndex"
+        :item="item"
+      />
+    </block>
+  </div>
+</template>
+
+<script>
+import test from './test'
+
+export default {
+  data () {
+    return {
+      currentIndex: 0,
+      list: (()=>{
+        let temp = []
+        for (let i=0;i<100;i++){
+          temp.push(i)
+        }
+        return temp
+      })()
+    }
+  },
+
+  components: {
+    test
+  },
+
+  methods: {
+    clickHandle () {
+      this.currentIndex += 1
+    }
+  },
+}
+</script>
---
+++ src/pages/index/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:['item']
+}
+</script>
